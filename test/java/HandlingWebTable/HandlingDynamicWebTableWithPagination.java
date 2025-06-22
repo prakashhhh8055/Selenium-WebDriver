6fd76d2ac444929174696083fed866e3ae98fd33
Prakash <prakashgmalagi@gmail.com>
2025-06-22 10:21:36 +0530

    Handling Dynamic Web Table with Pagination Practice

---
+++ test/java/HandlingWebTable/HandlingDynamicWebTableWithPagination.java
@@ -0,0 +1,56 @@
+package HandlingWebTable;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HandlingDynamicWebTableWithPagination {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-infobars");
+		options.setExperimentalOption("prefs", new HashMap<String, Object>() {{
+		    put("credentials_enable_service", false);
+		    put("profile.password_manager_enabled", false);
+		}});
+		WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://superadmin.trulyfreehome.dev/index.php?/Superadmin");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("superadmin@gmail.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("superadmin");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//li[@class='menuPageName_products']")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//a[normalize-space()='STORE PRODUCTS']")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//li[@id='1']")).click();
+        Thread.sleep(5000);
+        String text = driver.findElement(By.xpath("//div[@id='big_table_info']")).getText();
+
+     // Example text: "Showing 1 to 20 of 843 entries"
+     int total_Pages = Integer.parseInt(
+         text.substring(text.indexOf("of ") + 3, text.indexOf("entries")).trim()
+     );
+
+     System.out.println(total_Pages); // Outputs: 843
+
+        for(int i=2;i<=4;i++)
+        {
+        	driver.findElement(By.xpath("//div[@id='big_table_paginate']//a[.='"+i+"']")).click();
+        	Thread.sleep(3000);
+        }
+        
+        
+        
+
+	}
+
+}
